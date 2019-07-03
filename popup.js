350985846f6bae675c2c690f3df360230d17d113
kchateau <chateau14@outlook.com>
2019-07-02 20:03:52 -0700

    Update CSS to use Bulma
    Changed the header, search bar, add button, and list to use Bulma. This commit contains to functionality changes, just design.

--- popup.js
+++ popup.js
@@ -3,47 +3,52 @@ const URL_list = "urllist"
 // storage["URL_list"] = ["TEST-TEST-TEST"];
 
 //runs when chrome extension is loaded - use to load in URL's from storage
-window.onload=function(){
-  chrome.storage.local.get(URL_list, function(storage){
-    if(storage[URL_list] == null){
-      alert("making list empty");
-      url_storage[URL_list] = [];
-    };
-    url_storage = storage;
-    alert(JSON.stringify(storage));
-    // for(var i = 0; i < url_storage.length; i++){
-    //   document.getElementById("url_list").appendChild(url_storage[i]);
-    //   alert("added " + url_storage[i] + " to list");
-    // }
-    for(var url of url_storage[URL_list]){
-      var li = document.createElement("li");  //Create list item
-      var x = document.createTextNode(url);   //Create text node for URL to be added
-      var br = document.createElement("br");  //Break line to separate list items
-      li.appendChild(x);                      //Append URL to list item
-      document.getElementById("url_list").appendChild(li);  //Append list item to list
-      document.getElementById("url_list").appendChild(br);  //Append break line
-    }
-  });
-}
+// window.onload=function(){
+//   chrome.storage.local.get(URL_list, function(storage){
+//     if(storage[URL_list] == null){
+//       alert("making list empty");
+//       url_storage[URL_list] = [];
+//     };
+//     url_storage = storage;
+//     alert(JSON.stringify(storage));
+//     // for(var i = 0; i < url_storage.length; i++){
+//     //   document.getElementById("url_list").appendChild(url_storage[i]);
+//     //   alert("added " + url_storage[i] + " to list");
+//     // }
+//     for(var url of url_storage[URL_list]){
+//       var li = document.createElement("li");  //Create list item
+//       var x = document.createTextNode(url);   //Create text node for URL to be added
+//       var br = document.createElement("br");  //Break line to separate list items
+//       li.appendChild(x);                      //Append URL to list item
+//       document.getElementById("url_list").appendChild(li);  //Append list item to list
+//       document.getElementById("url_list").appendChild(br);  //Append break line
+//     }
+//   });
+// }
 
-// Add URL to list
+//Add URL to list
 function addUrl(){
-    //var ul = document.getElementById("list"); //Create List
-    var br = document.createElement("br");
-    var li = document.createElement("li");    //Create List Item
-    var new_url = document.getElementById("to_add").value; //Get item value from the "Add" button
-    var t = document.createTextNode(new_url); //Create URL from item value
-    li.appendChild(t); //Append URL to list
+    //Find table and create row and cells for url and delete button
+    var table = document.getElementById("url_list");
+    var row = table.insertRow();
+    var url_cell = row.insertCell(0);
+    var delete_button_cell = row.insertCell(1);
 
-    // TODO: Check if URL is in correct format - if not fix or error msg
-    
+    //get url to be added to table
+    var new_url = document.getElementById("to_add").value;
+
+    //create delete button
+    var button = document.createElement("button");
+    button.innerHTML = '-';
+
+    // FIXME: Check if URL is in correct format - if not fix or error msg
+
     //Check if URl is empty
     if(new_url == ""){
       alert("Cannot add an empty value!");
     } else {
-      document.getElementById("url_list").appendChild(li);
-      document.getElementById("url_list").appendChild(br);
-      url_storage[URL_list].push(new_url);
+      url_cell.innerHTML = (new_url);
+      delete_button_cell.appendChild(button);
     }
     document.getElementById("to_add").value = ""; // Clear text url from box after
     };
