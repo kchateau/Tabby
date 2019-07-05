fa0a18d96d919250c0ddd6fe8135f8af45cd0a4c
kchateau <chateau14@outlook.com>
2019-07-04 20:36:11 -0700

    Add helper functions
    Added helper functions to replace duplicated code and move event listeners all together at the bottom.

--- popup.js
+++ popup.js
@@ -10,57 +10,42 @@ window.onload=function(){
     url_storage = storage;
 
     for(var url of url_storage[URL_list]){
-      var table = document.getElementById("url_list");
-      var row = table.insertRow();
-      var url_cell = row.insertCell(0);
-      url_cell.id = "url_id"
-      var delete_button_cell = row.insertCell(1);
-
-      //get url to be added to table
-      // var new_url = document.getElementById("to_add").value;
-
+      table = createTable();
+      row = createRow(table);
+      cell = createCell(row);
+      button_cell = createButtonCell(row);
       deleteButton = createDeleteButton();
-
-      url_cell.innerHTML = (url);
-      delete_button_cell.appendChild(deleteButton);
+      cell.innerHTML = (url);
+      button_cell.appendChild(deleteButton);
     }
   });
 }
-//Add URL to list
+//Add URL and delete button to list
 function addUrl(){
-    //Find table and create row and cells for url and delete button
-    // TODO: make following into a function
-    var table = document.getElementById("url_list");
-    var row = table.insertRow();
-    var url_cell = row.insertCell(0);
-    url_cell.id = "url_id"
-    var delete_button_cell = row.insertCell(1);
+    //Create table, rows, cells
+    table = createTable();
+    row = createRow(table);
+    cell = createCell(row);
+    button_cell = row.insertCell(1);
 
-    //get url to be added to table
-    var new_url = document.getElementById("to_add").value;
-
+    new_url = document.getElementById("to_add").value;
     deleteButton = createDeleteButton();
 
-    // FIXME: Check if URL is in correct format - if not fix or error msg
-
-    //Check if URl is empty
     if(new_url == ""){
       alert("Cannot add an empty value!");
     } else {
-      url_cell.innerHTML = (new_url);
-      delete_button_cell.appendChild(deleteButton);
+      cell.innerHTML = (new_url);
+      button_cell.appendChild(deleteButton);
       url_storage[URL_list].push(new_url);
     }
     document.getElementById("to_add").value = ""; // Clear text url from box after
     saveUrl();
     };
-  document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('add_url').addEventListener('click', addUrl);
-});
+  
 
 // Open Tabs
 function openTabs(){ 
-  var table = document.getElementById("url_list");
+  var table = createTable();
   for(var i = 0; i < table.rows.length; i++){
     var url = table.rows[i].cells[0];
     chrome.tabs.create({
@@ -68,9 +53,6 @@ function openTabs(){
     });
   }
 }
-document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('open_tabs').addEventListener('click', openTabs);
-});
 //Button to save the URL's the user adds 
 function saveUrl(){
   chrome.storage.local.set(url_storage, function(){
@@ -88,6 +70,8 @@ function deleteURL(the_url){
   }
   saveUrl();
 }
+
+//Helper functions and event listeners
 function createDeleteButton(){
   var button = document.createElement("button");
   button.setAttribute('class', 'button is-small is-outlined is-warning');
@@ -97,3 +81,27 @@ function createDeleteButton(){
   }
   return button;
 }
+function createTable(){
+  var table = document.getElementById("url_list");
+  return table;
+}
+function createRow(table){
+  var row = table.insertRow();
+  return row;
+}
+function createCell(row){
+  var cell = row.insertCell(0);
+  cell.id = "url_id";
+  return cell;
+}
+function createButtonCell(row){
+  var button_cell = row.insertCell(1);
+  return button_cell;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('add_url').addEventListener('click', addUrl);
+});
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('open_tabs').addEventListener('click', openTabs);
+});
